09a55e65d740cf7339d1f72fe42749bbe0e53203
amtrevisan <alex.morales8@upr.edu>
2025-04-12 12:29:09 -0400

    Idle, Jump and fall Animations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,24 +7,31 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 10f; 
     private bool isFacingRight = true;
     public float jumpForce = 12f;
-
-    [SerializeField] private Rigidbody2D rb; // Reference to  the rigidBody attatched to the player
+    [SerializeField] private Rigidbody2D rb; 
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer; 
+    [SerializeField] private Animator animator;
 
     // Update is called once per frame
     void Update()
     {
+        // Player movement inputs
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
+        // Fast Fall mechanic
         float fallSpeed = (vertical < 0) ? -speed : rb.linearVelocity.y;
         rb.linearVelocity = new Vector2(horizontal * speed, fallSpeed);
+        // Check if sprite flip is necesarry accordint to movement
         if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f){
             Flip();
         }
+        // Jump check
         if (Input.GetButtonDown("Jump") && isGrounded()){
             Jump();
         }
+        // Animation parameters for state switching
+        animator.SetFloat("yVelocity", rb.linearVelocity.y);
+        animator.SetBool("isGrounded", isGrounded());
     }
     void Flip(){
         isFacingRight = !isFacingRight;
@@ -32,6 +39,7 @@ public class PlayerMovement : MonoBehaviour
         scale.x *= -1f; // flip x
         transform.localScale = scale; // apply it
     }
+    // Checks if the player is touching the ground using an object under its feet
     private bool isGrounded(){
         return Physics2D.OverlapCircle(groundCheck.position,0.2f,groundLayer);
     }
