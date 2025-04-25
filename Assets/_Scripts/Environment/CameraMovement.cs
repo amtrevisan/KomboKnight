68fc2e05c1e055990fc7653103cc381c0521ed5c
amtrevisan <alex.morales8@upr.edu>
2025-04-25 14:17:41 -0400

    Smooth Camera Following

--- Assets/_Scripts/Environment/CameraMovement.cs
+++ Assets/_Scripts/Environment/CameraMovement.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour
 {
     [SerializeField] Transform player;
-    
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private float smoothSpeed = 0.125f;
+    [SerializeField] private Vector3 offset;
+
+    void LateUpdate()
     {
-        // Follow player logic
-        transform.position = new Vector3(player.position.x, player.position.y + 2, -10);
+        Vector3 targetPosition = new Vector3(player.position.x + offset.x, -0.6f, -10f);
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, targetPosition, smoothSpeed * Time.deltaTime);
+        transform.position = smoothedPosition;
     }
 }
