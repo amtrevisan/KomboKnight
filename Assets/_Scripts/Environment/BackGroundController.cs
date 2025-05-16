2780af81654c21183b251909208eb7e0b8ba30af
amtrevisan <alex.morales8@upr.edu>
2025-05-16 11:29:32 -0400

    New Parallax BG

--- Assets/_Scripts/Environment/BackGroundController.cs
+++ Assets/_Scripts/Environment/BackGroundController.cs
@@ -14,22 +14,18 @@ public class BackGroundController : MonoBehaviour
         length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    void FixedUpdate()
+    void LateUpdate()
     {
         float distance = cam.transform.position.x * parallaxEffect;
         float movement = cam.transform.position.x * (1 - parallaxEffect);
-
         transform.position = new Vector3(startPos + distance, transform.position.y, transform.position.z);
 
-        if(movement > startPos + length){
+        if (movement > startPos + length)
+        {
             startPos += length;
         }
-        else if (movement < startPos - length){
+        else if (movement < startPos - length)
+        {
             startPos -= length;
         }
     }
